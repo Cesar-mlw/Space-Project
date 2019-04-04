afdc0a94756c36232f32378d0179d333e510a62f
Cesar Moura Leite Westphal <cesarmlwestphal@gmail.com>
2019-04-03 22:23:39 -0300

    Added Missile to level 2

---
+++ Assets/Scripts/RadarMissile.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadarMissile : MonoBehaviour
+{
+    public Missile[] missiles;
+    int index = 0;
+    GameObject lastTarget;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire2"))
+        {
+            if(lastTarget != null)
+            {
+                missiles[index].SetTarget(lastTarget);
+            }
+            else
+            {
+                missiles[index].SetTarget();
+            }
+            index++;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "enemy")
+        {
+            lastTarget = other.gameObject;
+            print(other.gameObject.name);
+        }
+        
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "enemy")
+        {
+            lastTarget = null;
+        }
+        
+    }
+}
